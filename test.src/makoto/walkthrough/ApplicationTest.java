94e7305f2e3e3301b044ee09984414971b103793
Brent-Milan <Bmilan24@gmail.com>
2017-08-21 12:08:43 -0400

    building a unit test example

--- test.src/makoto/walkthrough/ApplicationTest.java
+++ test.src/makoto/walkthrough/ApplicationTest.java
@@ -6,15 +6,36 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class ApplicationTest {
+	
+	private Application classUnderTest;
 
 	@Before
 	public void setUp() throws Exception {
+		classUnderTest = new Application();
 	}
+	
 
 	@Test
 	public void testAdd() {
-		fail("Not yet implemented");
-	}
+		long result = 10 + 23 + (-1) + 16;
+		
+		//compare
+		assertEquals(result, classUnderTest.add(10, 23, -1, 16));
+		
+		result = 100 + 102;
+		
+		//compare
+		assertEquals(result, classUnderTest.add(100, 102));
+		
+		result = 0;
+		
+		//compare
+		assertEquals(result, classUnderTest.add(0));
+	}	
+	
+	
+	
+	
 	
 	@Test
 	public void testSubtract() {
